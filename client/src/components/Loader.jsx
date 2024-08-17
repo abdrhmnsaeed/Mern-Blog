0aa4423a6235f8b51cff4e70e65504c94aa25a38
abdrhmnsaeed <abdrhmnsaeed@gmail.com>
2024-08-17 07:01:46 +0100

    added loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import LoadingGif from '../images/loading.gif'
+import LoadingGif from '../images/loader.gif'
 
 const Loader = () => {
   return (
     <div className='loader'>
-        <div className="loader__image">
-            {/* <img src={LoadingGif} alt="" /> */}
-        </div>
+      <div className="loader__image">
+        <img src={LoadingGif} alt="" />
+      </div>
     </div>
   )
 }
